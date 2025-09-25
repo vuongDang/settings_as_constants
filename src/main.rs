472096b0f2c0fa8d7b2bfc47fb34916eef570bcc
vuongDang <vuongdang9@gmail.com>
2025-09-25 14:33:00 +0200

    Branch condition on variables from environment gets optimized

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 fn main() {
     let foo = env!("TEMP_FOO");
     let bar = env!("TEMP_BAR");
-    println!("foo: {foo}");
-    println!("bar: {bar}");
+    // println!("foo: {foo}");
+    // println!("bar: {bar}");
+
+    if true {
+        print!("Witness true")
+    } else {
+        print!("Witness false")
+    }
 
     if foo == "true" {
         println!("I should be print this");
